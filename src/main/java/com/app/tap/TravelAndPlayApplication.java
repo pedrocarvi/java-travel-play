4a180fcce8134fb94d627c445fbc8dfb4465d7a3
Alfffos <alfonsogaray03@gmail.com>
2023-10-03 18:12:29 -0300

    first commit

---
+++ src/main/java/com/app/tap/TravelAndPlayApplication.java
@@ -0,0 +1,13 @@
+package com.app.tap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAndPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAndPlayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/tap/TravelAndPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.tap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAndPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
